c085ed0a305d1e2f18748772ecf06b33956ad89a
Saksham Garg <gargsaksham1895@gmail.com>
2026-04-29 00:08:46 +0530

    we dont know whats gonna happen next

--- src/hms/dao/DBConnection.java
+++ src/hms/dao/DBConnection.java
@@ -8,7 +8,7 @@ public class DBConnection {
 
     private static final String URL      = "jdbc:mysql://roundhouse.proxy.rlwy.net:41959/railway?useSSL=false&allowPublicKeyRetrieval=true&serverTimezone=UTC";
     private static final String USER     = "root";
-    private static final String PASSWORD = "PASSWORD_HERE";
+    private static final String PASSWORD = "YOUR PAsssword here";
 
     private static Connection connection = null;
 
